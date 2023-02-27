15ca943b742f0b77d24436c83fa29cb69e803358
Ákos Ferenc Kalamár <akos@Akoss-MacBook-Air.local>
2023-02-28 00:51:06 +0100

    picture fix

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -5,11 +5,9 @@ function MainPage() {
   return (
     <div className="main_page__container">
       <p>
-        {" "}
-        <Link to={"/resume"} style={{ textDecoration: "none" }}>
-          {" "}
-          Hej! Jag heter Ákos, trevligt att träffas!{" "}
-        </Link>
+        {/* <Link to={"/resume"} style={{ textDecoration: "none" }}> */}
+        Hej! Jag heter Ákos, trevligt att träffas!
+        {/* </Link> */}
       </p>
     </div>
   );
--- src/components/picture/Pictures.tsx
+++ src/components/picture/Pictures.tsx
@@ -6,12 +6,18 @@ type PictureProps = {
 };
 
 function Pictures(props: PictureProps) {
+  const url = process.env.PUBLIC_URL + "/imgs/AFK_cv-1.jpg";
   return (
     <Card className={`picture__container ${props.className}`}>
       <div
         className="picture__main"
         style={{
-          background: `url(${process.env.PUBLIC_URL}/imgs/AFK_cv-1.jpg)`,
+          background: `url(${url})`,
+          backgroundPosition: "center",
+          backgroundPositionY: "top",
+          backgroundOrigin: "border-box",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
         }}
       ></div>
     </Card>
